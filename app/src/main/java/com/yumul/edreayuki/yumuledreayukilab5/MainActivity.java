9087d7d5794d5d3993a62d13b0e837aed084f087
Yuki <edreayuki.yumul.iics@ust.edu.ph>
2018-10-09 00:14:40 +0800

    -

--- app/src/main/java/com/yumul/edreayuki/yumuledreayukilab5/MainActivity.java
+++ app/src/main/java/com/yumul/edreayuki/yumuledreayukilab5/MainActivity.java
@@ -1,5 +1,6 @@
 package com.yumul.edreayuki.yumuledreayukilab5;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -19,5 +20,13 @@ public class MainActivity extends AppCompatActivity {
             i = new Intent (this, Activity2.class);
             startActivity(i);
         }
+    }
+
+    public void map1 (View v){
+        Uri location = Uri.parse("geo:14.583709,121.057225");
+        Intent mapIntent = new Intent(Intent.ACTION_SEND, location);
+        String title = "Select application";
+        Intent chooser = Intent.createChooser(mapIntent, title);
+        startActivity(chooser);
     }
 }
